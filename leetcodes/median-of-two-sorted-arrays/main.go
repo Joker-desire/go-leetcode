26a20fd2f5a572a82ebc066734ef6025787ada94
Joker-desire <yangyin1106@163.com>
2023-06-21 23:52:51 +0800

    寻找两个正序数组的中位数

--- leetcodes/median-of-two-sorted-arrays/main.go
+++ leetcodes/median-of-two-sorted-arrays/main.go
@@ -9,6 +9,8 @@ import (
 // 4. 寻找两个正序数组的中位数
 // 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
 // 算法的时间复杂度应该为 O(log (m+n)) 。
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/median-of-two-sorted-arrays
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums1 = append(nums1, nums2...)
